6101afa0b32c45eb71727cf1d8331a21ca8cbb01
kunlunrepo <482976531@qq.com>
2024-05-26 23:49:52 +0800

    函数组件使用useImmer操作数组中的对象值

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import {useState} from "react";
-
+import { useImmer } from "use-immer";
 const App = () => {
     const [name, setName] = useState("");
     // 数组
-    const [person, setPerson] = useState([{id: 0, name: "老王"}]);
+    const [person, setPerson] = useImmer([{id: 0, name: "老王", checked: false}]);
 
     const handleDelete = (item) => {
         setPerson(person.filter((person) => person.id !== item.id));
     };
+
+    const handleCheck = (item, checked) => {
+        setPerson((draft) => {
+            console.log(draft)
+            const checkedItem = draft.find((person) => person.id === item);
+            console.log(checkedItem)
+            checkedItem.checked = checked;
+        })
+        console.log(person)
+    }
 
     return (
         <>
@@ -15,7 +25,7 @@ const App = () => {
             <input value={name} onChange={(e) => setName(e.target.value)}/>
             <button
                 onClick={() => {
-                    setPerson([...person, {id: person.length, name: name}]);
+                    setPerson([...person, {id: person.length, name: name, checked: false}]);
                 }}
             >
                 添加
@@ -23,8 +33,13 @@ const App = () => {
             <ul>
                 {
                     person.map((item) => (
-                        <li key={item.id}>{item.name}
-                        <button onClick={()=> handleDelete(item)}>删除</button>
+                        <li key={item.id}>
+                            <label>
+                                <input type="checkbox" checked={item.checked}
+                                       onChange={(e) => handleCheck(item.id, e.target.checked)}/>
+                            </label>
+                            {item.name}
+                            <button onClick={() => handleDelete(item)}>删除</button>
                         </li>
                     ))
                 }
