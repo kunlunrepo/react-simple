52591bd3b86d83c37f33c633cfc11293810344ee
kunlunrepo <482976531@qq.com>
2024-05-26 16:33:52 +0800

    ref的回调用法

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,26 @@ import React from "react";
 
 class App extends React.Component {
 
-    /**
-     * 弹窗方法
-     */
+    state = {
+        xd: true
+    }
+
     popClick = () => {
         // console.log(document.getElementsByTagName('input')[0].value);
-        const { inputRef} = this.refs;
-        console.log(inputRef.value)
+
+        // const { inputRef} = this.refs;
+        // console.log(inputRef.value)
+
+        console.log(this.inputRef.value)
     }
+
+    handleClick = () => {
+        this.setState({
+            xd: !this.state.xd
+        })
+    }
+
+    refFun = (e) => ((this.inputRef = e), console.log("ref执行了"))
 
     /**
      * 渲染
@@ -21,8 +33,12 @@ class App extends React.Component {
     render() {
         return (
             <>
-                <input type="text" placeholder="请输入内容" ref="inputRef"/>
+                {/*<input type="text" placeholder="请输入内容" ref={
+                    (e) => ((this.inputRef = e), console.log("ref执行了"))
+                }/>*/}
+                <input type="text" placeholder="请输入内容" ref={this.refFun}/>
                 <button onClick={this.popClick}>点击生成弹窗</button>
+                <button onClick={this.handleClick}>点击更新组件</button>
             </>
         )
     }
