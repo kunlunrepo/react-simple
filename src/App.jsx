8d4d24b5cc501149682d20fee9f0749e5341f2d3
kunlunrepo <482976531@qq.com>
2024-05-26 23:13:03 +0800

    函数组件使用useState的复杂对象

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,28 @@ const App = () => {
     const [message, setMessage] = useState({
         name: "旧课程",
         website: "www.baidu.com",
+        staff: {
+            name: "老王",
+            hobby: "按摩"
+        }
     });
 
     const changeName = () => {
-        setMessage({ ...message, name: "新课程" });
+        setMessage({ ...message, name: "新课程" }); // 展开运算符相当于浅拷贝
+    };
+
+    const changeHobby = () => {
+        setMessage({ ...message, staff: { ...message.staff, hobby: "洗脚" } }); // 复杂对象需要多次展开
     };
 
     return (
         <div>
             <h1>{message.name}</h1>
             <h1>{message.website}</h1>
+            <h1>{message.staff.name}</h1>
+            <h1>{message.staff.hobby}</h1>
             <button onClick={changeName}>更改title</button>
+            <button onClick={changeHobby}>更改hobby</button>
         </div>
     );
 };
