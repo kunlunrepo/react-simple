e69cf179a3e49ba024bf9b9a18eb486aead24d85
kunlunrepo <482976531@qq.com>
2024-05-26 16:45:07 +0800

    ref的createRef用法

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,15 @@ import React from "react";
 
 class App extends React.Component {
 
-    state = {
-        xd: true
-    }
+    // 创建容器
+    inputRef = React.createRef();
+    inputRef2 = React.createRef(); // 不要过度使用ref
 
     popClick = () => {
-        // console.log(document.getElementsByTagName('input')[0].value);
-
-        // const { inputRef} = this.refs;
-        // console.log(inputRef.value)
-
-        console.log(this.inputRef.value)
+        console.log(this)
+        console.log(this.inputRef.current.value)
+        console.log(this.inputRef2.current.value)
     }
-
-    handleClick = () => {
-        this.setState({
-            xd: !this.state.xd
-        })
-    }
-
-    refFun = (e) => ((this.inputRef = e), console.log("ref执行了"))
 
     /**
      * 渲染
@@ -36,9 +25,9 @@ class App extends React.Component {
                 {/*<input type="text" placeholder="请输入内容" ref={
                     (e) => ((this.inputRef = e), console.log("ref执行了"))
                 }/>*/}
-                <input type="text" placeholder="请输入内容" ref={this.refFun}/>
+                <input type="text" placeholder="请输入内容" ref={this.inputRef}/>
+                <input type="text" placeholder="请输入内容" ref={this.inputRef2}/>
                 <button onClick={this.popClick}>点击生成弹窗</button>
-                <button onClick={this.handleClick}>点击更新组件</button>
             </>
         )
     }
