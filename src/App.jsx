440fffe9d272bea93c3afc031c02435449135194
kunlunrepo <482976531@qq.com>
2024-05-26 23:22:40 +0800

    函数组件使用useState操作数组

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,29 @@
-import {useImmer} from  "use-immer"
+import {useState} from "react";
 
 const App = () => {
-    const [message, setMessage] = useImmer({
-        name: "旧课程",
-        website: "www.baidu.com",
-        staff: {
-            name: "老王",
-            hobby: "按摩"
-        }
-    });
-
-    const changeName = () => {
-        // setMessage({ ...message, name: "新课程" }); // 展开运算符相当于浅拷贝
-
-        setMessage((draft) => {
-            draft.name = "新课程";
-        });
-    };
-
-    const changeHobby = () => {
-        // setMessage({ ...message, staff: { ...message.staff, hobby: "洗脚" } }); // 复杂对象需要多次展开
-        setMessage((draft) => {
-            draft.staff.hobby = "洗脚";
-        });
-    };
+    const [name, setName] = useState("");
+    const [person, setPerson] = useState([{id: 0, name: "老王"}]);
 
     return (
-        <div>
-            <h1>{message.name}</h1>
-            <h1>{message.website}</h1>
-            <h1>{message.staff.name}</h1>
-            <h1>{message.staff.hobby}</h1>
-            <button onClick={changeName}>更改title</button>
-            <button onClick={changeHobby}>更改hobby</button>
-        </div>
+        <>
+            <h1>员工列表：</h1>
+            <input value={name} onChange={(e) => setName(e.target.value)}/>
+            <button
+                onClick={() => {
+                    setPerson([...person, {id: person.length, name: name}]);
+                }}
+            >
+                添加
+            </button>
+            <ul>
+                {
+                    person.map((item) => (
+                        <li key={item.id}>{item.name}</li>
+                    ))
+                }
+            </ul>
+        </>
     );
 };
+
 export default App;
